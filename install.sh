9416020c1d95fa444aebd282f1086331b732788f
Lyuben Blagoev <lblagoev@gmail.com>
2013-08-13 08:14:20 +0300

    Fix several issues

--- install.sh
+++ install.sh
@@ -4,10 +4,8 @@ set -e
 
 dotfiles_path=$HOME/dotfiles
 
-export dotfiles_path
-
-command -v git > dev/null || { echo 'GIT is required but not found.'; exit 1 }
-command -v vim > dev/null || { echo 'VIM is required but not found.'; exit 1 }
+command -v git > /dev/null || { echo 'GIT is required but not found.'; exit 1; }
+command -v vim > /dev/null || { echo 'VIM is required but not found.'; exit 1; }
 
 if [[ ! -d $dotfiles_path ]]; then 
 	mkdir -p $dotfiles_path
@@ -16,4 +14,4 @@ fi
 git clone https://github.com/lyubenblagoev/dotfiles $dotfiles_path
 
 chmod 700 $dotfiles_path/configure.sh
-$dotfiles_path/configure.sh
+echo "$dotfiles_path/configure.sh"
