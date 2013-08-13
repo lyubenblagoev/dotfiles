a47cc456237ac98cba82adafed54c0ad50bb0888
Lyuben Blagoev <lblagoev@gmail.com>
2013-08-13 09:00:29 +0300

    make install script use the correct branch

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ if [[ ! -d $dotfiles_path ]]; then
 	mkdir -p $dotfiles_path
 fi 
 
-git clone https://github.com/lyubenblagoev/dotfiles $dotfiles_path
+git clone https://github.com/lyubenblagoev/features/webinstall/dotfiles $dotfiles_path
 
 chmod 700 $dotfiles_path/configure.sh
 echo "$dotfiles_path/configure.sh"
