0c2fba0cbd7af169d74bec4d9d30f0e0b0b46730
Lyuben Blagoev <lblagoev@gmail.com>
2013-08-13 16:19:07 +0300

    Restrict execute access only to the owner

--- install.sh
+++ install.sh
@@ -12,9 +12,9 @@ if [[ ! -d $dotfiles_path ]]; then
     mkdir -p $dotfiles_path
 fi 
 
-git clone https://github.com/lyubenblagoev/dotfiles $dotfiles_path
-
 cd $dotfiles_path
 umask 022
+git clone https://github.com/lyubenblagoev/dotfiles .
+chmod 700 configure.sh install.sh
 ./configure.sh $dotfiles_path
 cd $initial_path
