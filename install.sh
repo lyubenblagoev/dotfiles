78dbe01bcce80d5a5b212974fafb00058717bc13
Lyuben Blagoev <lblagoev@gmail.com>
2013-08-14 12:21:20 +0300

    Enable update on already installed dotfiles and minor improvements to the code

--- install.sh
+++ install.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-set -e
-
 DOT_DIR=$HOME/.dotfiles
 INITIAL_DIR=`pwd`
 
-command -v git > /dev/null || { echo 'GIT is required but not found.'; exit 1; }
-command -v vim > /dev/null || { echo 'VIM is required but not found.'; exit 1; }
+command -v git > /dev/null
+if [[ $? != 0 ]]; then
+    echo 'Git is required, but not found. Aborting.' >&2
+    exit 1
+fi 
+command -v vim > /dev/null
+if [[ $? != 0 ]]; then
+    echo 'Vim is required, but not found. Aborting.' >&2
+    exit 1
+fi
 
 if [[ ! -d $DOT_DIR ]]; then 
     mkdir -p $DOT_DIR
-fi 
+    cd $DOT_DIR && git clone https://github.com/lyubenblagoev/dotfiles .
+else
+    echo "Dotfiles is already installed in $DOT_DIR, trying to update"
+    cd $DOT_DIR && git pull
+fi
 
-cd $DOT_DIR
-umask 022
-git clone https://github.com/lyubenblagoev/dotfiles .
 chmod 700 configure.sh install.sh
 ./configure.sh $DOT_DIR
+
 cd $INITIAL_DIR
