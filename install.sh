5699477a01a53b61becaf43a55e89de74f96bc16
Lyuben Blagoev <lblagoev@gmail.com>
2024-12-21 14:45:12 +0200

    Update directory structure and add bash configuration

--- install.sh
+++ install.sh
@@ -8,30 +8,41 @@ if [[ -d ~/.dotfiles ]]; then
 fi
 
 echo "Cloning lyubenblagoev/dotfiles into ~/.dotfiles"
-git clone http://github.com/lyubenblagoev/dotfiles.git ~/.dotfiles
+git clone https://github.com/lyubenblagoev/dotfiles.git ~/.dotfiles
+
+if ! grep -i .dotfiles\/config\/bash\/environment ~/.bashrc > /dev/null; then
+    sed -i "$ a [ -r ~/.dotfiles/config/bash/environment ] && . ~/.dotfiles/config/bash/environment" ~/.bashrc
+fi
 
 echo "Cloning Vundle into vim/bundle"
-mkdir -p ~/.dotfiles/vim/bundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.dotfiles/vim/bundle/Vundle.vim
+mkdir -p ~/.dotfiles/config/local/vim/bundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.dotfiles/config/local/vim/bundle/Vundle.vim
 
 echo "Creating symbolic links"
+
 if [[ -d ~/.vim ]]; then
     echo "Directory ~/.vim already exists, backing up to ~/.vim.bak"
-    mv .vim ~/.vim.bak
+    mv ~/.vim ~/.vim.bak
+elif [[ -L ~/.vim ]]; then
+    rm ~/.vim
 fi
-ln -s ~/.dotfiles/vim ~/.vim
+ln -s ~/.dotfiles/config/local/vim ~/.vim
 
 if [[ -e ~/.vimrc ]]; then
     echo "Backing up .vimrc to .vimrc.bak"
-    mv .vimrc .vimrc.bak
+    mv ~/.vimrc ~/.vimrc.bak
+elif [[ -L ~/.vimrc ]]; then
+    rm ~/.vimrc
 fi
-ln -s ~/.dotfiles/vimrc ~/.vimrc
+ln -s ~/.dotfiles/config/vim/vimrc ~/.vimrc
 
 if [[ -e ~/.gitconfig ]]; then
     echo "Backing up .gitconfig to .gitconfig.bak"
-    mv .gitconfig .gitconfig.bak
+    mv ~/.gitconfig ~/.gitconfig.bak
+elif [[ -L ~/.gitconfig ]]; then
+    rm ~/.gitignore
 fi
-ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/config/git/gitconfig ~/.gitconfig
 
 echo "Installing Vim plugins"
 vim +PluginInstall +qa!
