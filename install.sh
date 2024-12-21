fdf28db05bf5480d30553d7fb84a5b29a7bb9006
Lyuben Blagoev <lblagoev@gmail.com>
2024-12-21 17:50:59 +0200

    Improve install script

--- install.sh
+++ install.sh
@@ -1,50 +1,57 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 if [[ -d ~/.dotfiles ]]; then
-    echo "Directory ~/.dotfiles already exists, skip installation"
-    exit 0
+    echo "==> ~/.dotfiles already exists, update and reinstall"
+    rm -rf ~/.dotfiles/config/local
+    cd ~/.dotfiles
+    git pull origin master
+    cd -
+else
+    echo "==> Cloning lyubenblagoev/dotfiles into ~/.dotfiles"
+    git clone https://github.com/lyubenblagoev/dotfiles.git ~/.dotfiles
 fi
 
-echo "Cloning lyubenblagoev/dotfiles into ~/.dotfiles"
-git clone https://github.com/lyubenblagoev/dotfiles.git ~/.dotfiles
-
 if ! grep -i .dotfiles\/config\/bash\/environment ~/.bashrc > /dev/null; then
+    echo "==> Setting-up Bash environment"
     sed -i "$ a [ -r ~/.dotfiles/config/bash/environment ] && . ~/.dotfiles/config/bash/environment" ~/.bashrc
 fi
 
-echo "Cloning Vundle into vim/bundle"
+echo "==> Cloning Vundle into vim/bundle"
 mkdir -p ~/.dotfiles/config/local/vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.dotfiles/config/local/vim/bundle/Vundle.vim
 
-echo "Creating symbolic links"
+echo "==> Creating symbolic links"
 
+# .vim directory symlink
 if [[ -d ~/.vim ]]; then
-    echo "Directory ~/.vim already exists, backing up to ~/.vim.bak"
+    echo " |-- ~/.vim has been renamed to ~/.vim.bak"
     mv ~/.vim ~/.vim.bak
 elif [[ -L ~/.vim ]]; then
     rm ~/.vim
 fi
 ln -s ~/.dotfiles/config/local/vim ~/.vim
 
+# .vimrc symlink
 if [[ -e ~/.vimrc ]]; then
-    echo "Backing up .vimrc to .vimrc.bak"
+    echo " |-- ~/.vimrc has been renamed to .vimrc.bak"
     mv ~/.vimrc ~/.vimrc.bak
 elif [[ -L ~/.vimrc ]]; then
     rm ~/.vimrc
 fi
 ln -s ~/.dotfiles/config/vim/vimrc ~/.vimrc
 
+# .gitconfig symlink
 if [[ -e ~/.gitconfig ]]; then
-    echo "Backing up .gitconfig to .gitconfig.bak"
+    echo " |-- ~/.gitconifg has been renamed to .gitconfig.bak"
     mv ~/.gitconfig ~/.gitconfig.bak
 elif [[ -L ~/.gitconfig ]]; then
     rm ~/.gitconfig
 fi
 ln -s ~/.dotfiles/config/git/gitconfig ~/.gitconfig
 
-echo "Installing Vim plugins"
+echo "==> Installing Vim plugins"
 vim +PluginInstall +qa!
 
-echo "Done"
+echo "==> Done"
